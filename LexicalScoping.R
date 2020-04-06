2f548b4e8d614c82d3c0df18bab4f01fa1490fe9
CantStopWontStop <soried@gmail.com>
2020-04-05 22:01:00 -0400

    Lexical Scoping Assignment

---
+++ LexicalScoping.R
@@ -0,0 +1,32 @@
+
+
+# This function creates a "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(x = matrix()) {
+  j <- NULL
+  set <- function(y){
+    x <<- y
+    j <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) j <<- inverse
+  getInverse <- function() j 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
+}
+
+# This function gets the inverse of the "matrix" created by makeCacheMatrix. 
+# It should retrieve the inverse from the cache if the inverse has already been calculated.
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  j <- x$getInverse()
+  if(!is.null(j)){
+    message("getting cached data")
+    return(j)
+  }
+  mat <- x$get()
+  j <- solve(mat,...)
+  x$setInverse(j)
+  j
+}
